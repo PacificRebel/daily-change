48f4bf66244332803792a750d90b7c445e3cf903
Heli Sivunen <heli@mooncat.tv>
2020-01-13 16:48:39 +0000

    Successfully added favicon :).

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'behaviours/index'
 
   resources :behaviours
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'behaviours#index'
 end
