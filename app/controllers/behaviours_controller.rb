3ec5d61789c4b23dcb7a432c714f16411c51f90e
Heli Sivunen <heli@mooncat.tv>
2020-01-15 09:26:08 +0000

    Able to create new behaviour.

--- app/controllers/behaviours_controller.rb
+++ app/controllers/behaviours_controller.rb
@@ -1,8 +1,26 @@
 class BehavioursController < ApplicationController
+
+  def show
+    @behaviour = Behaviour.find(params[:id])
+  end
+
+  def new
+  end
+
   def index
   end
 
   def create
-    render plain: params[:behaviour].inspect
+    @behaviour = Behaviour.new(params.require(:behaviour).permit(:title, :text))
+
+    @behaviour.save
+    redirect_to @behaviour
   end
+
+  private
+    def behaviour_params
+      params.require(:behaviour).permit(:title, :text)
+    end
 end
+
+  # render plain: params[:behaviour].inspect
